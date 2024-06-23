d80e4b2f8219f3b74dbfd31c7166eca1b08bfe94
Abdelrahman mahmoud <abdelrahman.mahmoud@hmaserv.com>
2024-06-23 11:47:42 +0300

    return just seo from layout controller

--- src/server/controller/layout.js
+++ src/server/controller/layout.js
@@ -7,48 +7,56 @@ const controllerWrapper = require("../utils/controllerWrapper");
 
 const handelGettingData = async (req, res, next) => {
 
-     const section = req.query.section;
+    const section = req.query.section;
     const data = await Settings.findAll();
-     if(section){
+    if (section) {
         switch (section) {
             case 'terms':
                 let termsData = {}
-                data.forEach((item)=>{
-                    if(item.key == 'termsAr') termsData.termsAr = item.value
-                    if(item.key == 'termsEn') termsData.termsEn = item.value
+                data.forEach((item) => {
+                    if (item.key == 'termsAr') termsData.termsAr = item.value
+                    if (item.key == 'termsEn') termsData.termsEn = item.value
                 })
                 successResponse(res, termsData);
             case "policy":
                 let policyData = {}
-                data.forEach((item)=>{
-                    if(item.key == 'policyAr') policyData.policyAr = item.value
-                    if(item.key == 'policyEn') policyData.policyEn = item.value
+                data.forEach((item) => {
+                    if (item.key == 'policyAr') policyData.policyAr = item.value
+                    if (item.key == 'policyEn') policyData.policyEn = item.value
                 })
                 successResponse(res, policyData);
             case "about":
                 let aboutData = {}
-                data.forEach((item)=>{
-                    if(item.key == 'aboutAr') aboutData.aboutAr = item.value
-                    if(item.key == 'aboutEn') aboutData.aboutEn = item.value
+                data.forEach((item) => {
+                    if (item.key == 'aboutAr') aboutData.aboutAr = item.value
+                    if (item.key == 'aboutEn') aboutData.aboutEn = item.value
                 })
                 successResponse(res, aboutData);
+            case "seo":
+                let seoData = {}
+                data.forEach((item) => {
+                    if (item.key == 'title') seoData.title = item.value
+                    if (item.key == 'description') seoData.description = item.value
+                    if (item.key == 'keyWords') seoData.keyWords = item.value
+                })
+                successResponse(res, seoData);
             default:
-            
-            }
-     }
+
+        }
+    }
 
     const layoutData = {
         data: {},
         seo: {},
         socialMedia: {},
-        pages:{},
+        // pages: {},
     }
 
     data.forEach((item, idx) => {
         let socialMedia = ['facebook', 'tiktok', 'instagram', 'x', 'youtube'];
         let seo = ['title', 'description', 'keyWords'];
-        let pages = ['aboutAr' , 'aboutEn' , 'policyAr' , 'policyEn', 'termsAr', 'termsEn' ];
-        let desc = [ 'mainDescAr', "mainDescEn", "footerDescAr", "footerDescEn" ]
+        let pages = ['aboutAr', 'aboutEn', 'policyAr', 'policyEn', 'termsAr', 'termsEn'];
+        let desc = ['mainDescAr', "mainDescEn", "footerDescAr", "footerDescEn"]
         if (socialMedia.includes(item.key)) {
             layoutData.socialMedia[item.key] = item.value
         } else if (seo.includes(item.key)) {
@@ -79,7 +87,7 @@ const updateLayout = controllerWrapper(async (req, res, next) => {
     // start validate
     await validationChecker(req, res);
 
-    const keysToUpdate =  Object.keys(req.body); // the keys for only updated values 
+    const keysToUpdate = Object.keys(req.body); // the keys for only updated values 
 
     // to get only updated values with its keys .
     const updates = [];
